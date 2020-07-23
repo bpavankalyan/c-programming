99c885db9799ceb3e710a6b51824e7330d8c5429
Coursera Learner <nobody@nowhere.nul>
2020-07-23 06:57:47 +0000

    added

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-size_t maxseq(int * arr, size_t n)
+size_t maxSeq(int * arr, size_t n)
 {
 
-  if (n=0)
+  if (n==0)
     return 0;
   int max=1,current=1;
   for(int i=1;i<n;i++)
@@ -13,7 +13,7 @@ size_t maxseq(int * arr, size_t n)
 	 current++;
 	 else
 	   current=1;
-	 if(max<cuurent)
+	 if(max<current)
 	   max=current;
         }
   return max;
