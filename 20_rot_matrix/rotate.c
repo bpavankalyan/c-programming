eb7ebf8dacb0ff56fe10d5d3d14a9afb8735371f
Coursera Learner <nobody@nowhere.nul>
2020-08-08 05:59:17 +0000

    add

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-void rotate(char rot[10][10])
-{
-  if(rot==NULL)
-    return ;
-  char rotma[10][10];
-  for(int i=0;i<10;i++)
-    for(int j=0;j<10;j++)
-      rotma[j][9-i]=rot[i][j];
-  for(int i=0;i<10;i++)
-    for(int j=0;j<10;j++)
-      rot[i][j]=rotma[i][j];  
+void rotate(char matrix[10][10]){
+  if(matrix!=NULL){
+    char rotMatrix[10][10];
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+	rotMatrix[j][10-1-i]=matrix[i][j];
+      }
+    }
+    for(int i=0;i<10;i++){
+      for(int j=0;j<10;j++){
+	matrix[i][j]=rotMatrix[i][j];
+      }
+    }
+  }
 }
